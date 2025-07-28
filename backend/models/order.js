d424a0be647497c1d311cc77eace529e7cc88bfd
Muzammil khan <khan@Muzammils-MacBook-Air.local>
2025-07-28 19:05:24 +0530

    chnages

--- backend/models/order.js
+++ backend/models/order.js
@@ -7,7 +7,16 @@ const orderSchema = new mongoose.Schema({
         default: 'PENDING'
     },
     trackingStage: String,
-    customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer' }
+    customer: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer' },
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product' 
+            },
+            quantity: Number
+        }
+    ]
 });
 
 module.exports = mongoose.model('Order', orderSchema);
